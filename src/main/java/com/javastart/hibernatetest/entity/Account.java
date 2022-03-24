d5fecd27fab2f99f077086ba270d57c02b18be48
Kukulin <dkukulin@gmail.com>
2022-03-24 12:46:37 +0100

    Initial commit

---
+++ src/main/java/com/javastart/hibernatetest/entity/Account.java
@@ -0,0 +1,68 @@
+package com.javastart.hibernatetest.entity;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.StringJoiner;
+
+@Entity
+public class Account
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long accountId;
+    private  String name;
+    private  Integer age;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<Bill> bills;
+
+    public Account(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Account() {
+
+    }
+
+    public List<Bill> getBills() {
+        return bills;
+    }
+
+    public void setBills(List<Bill> bills) {
+        this.bills = bills;
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Account.class.getSimpleName() + "[", "]")
+                .add("accountId=" + accountId)
+                .add("name='" + name + "'")
+                .add("age=" + age)
+                .toString();
+    }
+}
